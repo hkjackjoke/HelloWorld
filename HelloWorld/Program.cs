968409c1b80db5687612424892217c8e2248b2ae
a1 <a1@aaa.com>
2022-04-07 20:45:32 +1200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write($"\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
