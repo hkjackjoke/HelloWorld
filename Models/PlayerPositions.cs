28169ab61097865aab9738d5a73fc7caf6dcbfb2
a1 <a1@aaa.com>
2022-04-28 12:34:05 +1200

    debug use

---
+++ Models/PlayerPositions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootballPlayer.Models
+{
+    public enum PlayerPositions
+    {
+        Goalkeeper,
+        Defender,
+        Midfielder,
+        Forward
+    }
+}
